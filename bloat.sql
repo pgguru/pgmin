8091b77fb63c50da2c6c8a738a586fc303829ce6
David Christensen <david.christensen@crunchydata.com>
2022-10-05 12:58:03 -0400

    fix: include original query with sql comments

--- bloat.sql
+++ bloat.sql
@@ -1,8 +1,8 @@
 SELECT
-  current_database(), schemaname, tablename,
+  current_database(), schemaname, tablename, /*reltuples::bigint, relpages::bigint, otta,*/
   ROUND((CASE WHEN otta=0 THEN 0.0 ELSE sml.relpages::float/otta END)::numeric,1) AS tbloat,
   CASE WHEN relpages < otta THEN 0 ELSE bs*(sml.relpages-otta)::BIGINT END AS wastedbytes,
-  iname,
+  iname, /*ituples::bigint, ipages::bigint, iotta,*/
   ROUND((CASE WHEN iotta=0 OR ipages=0 THEN 0.0 ELSE ipages::float/iotta END)::numeric,1) AS ibloat,
   CASE WHEN ipages < iotta THEN 0 ELSE bs*(ipages-iotta) END AS wastedibytes
 FROM (
@@ -11,7 +11,7 @@ FROM (
     CEIL((cc.reltuples*((datahdr+ma-
       (CASE WHEN datahdr%ma=0 THEN ma ELSE datahdr%ma END))+nullhdr2+4))/(bs-20::float)) AS otta,
     COALESCE(c2.relname,'?') AS iname, COALESCE(c2.reltuples,0) AS ituples, COALESCE(c2.relpages,0) AS ipages,
-    COALESCE(CEIL((c2.reltuples*(datahdr-12))/(bs-20::float)),0) AS iotta
+    COALESCE(CEIL((c2.reltuples*(datahdr-12))/(bs-20::float)),0) AS iotta -- very rough approximation, assumes all cols
   FROM (
     SELECT
       ma,bs,schemaname,tablename,
@@ -29,10 +29,10 @@ FROM (
         ) AS nullhdr
       FROM pg_stats s, (
         SELECT
-          ( SELECT current_setting('block_size')::numeric) AS bs,
+          (SELECT current_setting('block_size')::numeric) AS bs,
           CASE WHEN substring(v,12,3) IN ('8.0','8.1','8.2') THEN 27 ELSE 23 END AS hdr,
           CASE WHEN v ~ 'mingw32' THEN 8 ELSE 4 END AS ma
-        FROM ( SELECT version() AS v) AS foo
+        FROM (SELECT version() AS v) AS foo
       ) AS constants
       GROUP BY 1,2,3,4,5
     ) AS foo
